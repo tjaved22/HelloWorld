a6d6d33b0059244d232f73bc012b21b6aee0b5a9
Talha Javed <talhajaveditdev@gmail.com>
2022-12-23 01:39:01 -0500

    Small edits

--- app.py
+++ app.py
@@ -127,13 +127,13 @@
 #         uniques.append(number)
 # print(uniques)
 
-phone = input("Phone: ")
-digits_mapping = {
-    "1": "One",
-    "2": "Two",
-    "3": "Three",
-    "4": "Four"
-}
+#phone = input("Phone: ")
+#digits_mapping = {
+#    "1": "One",
+#    "2": "Two",
+#    "3": "Three",
+#    "4": "Four"
+#}
 output = ''
 for ch in phone:
     output += digits_mapping.get(ch, "!") + ' '
